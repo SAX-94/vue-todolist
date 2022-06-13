0625e95ade0c5a38195dc973136acc1c2c3552f9
SAX_94 <matteosacchi94@gmail.com>
2022-06-13 19:36:58 +0200

    finished script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 const app = new Vue({
     el: "#app",
     data: {
-        todos: []
+        todos: [
+            {
+                text: "Preparare il porridge",
+                done: false,
+            },
+            {
+                text: "Preparare il caffé",
+                done: false,
+            },
+            {
+                text: "Lavarmi i denti",
+                done: false,
+            },
+            {
+                text: "Accendere il PC",
+                done: false,
+            },
+        ],
+        newTodo: "",
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.length > 1) {
+                this.todos.push({
+                    text: this.newTodo,
+                    done: false
+                });
+                this.newTodo = "";
+            }
+        },
+        removeTodo(index) {
+            if (confirm(`Sei sicuro di eliminare "${this.todos[index].text}"?`)) {
+                this.todos.splice(index, 1);
+            }
+        }
     }
 })
